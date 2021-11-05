e327bc15b49597cb4b715cd3ce6e98928c0ce6fe
muizayo1 <muiz1ayo@gmail.com>
2021-11-04 20:13:10 -1100

    first commit

---
+++ src/components/navigation.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <div className='container'>
+        <div className='nav-columns'>
+          <div className='nav-column'>
+            <div className='nav-label'>Menu</div>
+            <ul className='nav-links'>
+              <li>
+                <NavLink to='/case-studies' exact>
+                  Case Studies
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='/approach' exact>
+                  Approach
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='/services' exact>
+                  Services
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='/about-us' exact>
+                  About us
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className='nav-column'>
+            <div className='nav-label'>Contact</div>
+            <div className='nav-infos'>
+              <ul className='nav-info'>
+                <li className='nav-info-label'>Email</li>
+                <li>
+                  <NavLink to='/contact' exact>
+                    Get in touch with us
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to='/audit' exact>
+                    Get a free audit
+                  </NavLink>
+                </li>
+              </ul>
+              <ul className='nav-info'>
+                <li className='nav-info-label'>Headquarter</li>
+                <li>40,Opeyemi Street</li>
+                <li>Ajagba,Iyana church</li>
+                <li>Ibadan</li>
+              </ul>
+              <ul className='nav-info'>
+                <li className='nav-info-label'>Phone</li>
+                <li>+234 9060002718</li>
+              </ul>
+              <ul className='nav-info'>
+                <li className='nav-info-label'>Legal</li>
+                <li>Privacy & Cookies</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
